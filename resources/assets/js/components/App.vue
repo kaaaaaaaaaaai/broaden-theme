5809023ff11b912531d670ec3fff726204d6c3c5
kaiogita <kaiogita@gmail.com>
2018-04-23 02:07:41 +0900

    add entory point component

---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>
+            <router-link to="/top">トップページ</router-link>
+            <router-link to="/users">ユーザー一覧ページ</router-link>
+        </p>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Start"
+    }
+</script>
