378c6ddd8f91f8d3fbdde204546c9f637facbaeb
kai ogita <kai-ogita@rich.co.jp>
2018-04-25 13:27:34 +0900

    master:add item component

---
+++ resources/assets/js/components/Item.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img class="card-img-top" :src="data.picture" :alt="data.title">
+    <div class="card-body">
+      <a href="#" class="btn btn-primary">投票</a>
+      {{ data.time }}
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Item",
+        props: ["data"],
+        mounted() {
+            // props で受け取ったら自分のデータと同じように this で使用できるようになる
+            console.log(this.data)
+        }
+    }
+</script>
