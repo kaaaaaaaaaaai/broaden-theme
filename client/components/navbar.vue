f4992bf8afb0f924778ee1382d96d2028176530c
kaiogita <kaiogita@gmail.com>
2018-05-02 23:42:50 +0900

    navbar add toggle action

--- client/components/navbar.vue
+++ client/components/navbar.vue
@@ -1,19 +1,33 @@
 <template>
     <nav class="navbar is-white" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
-            <div class="navbar-item">Avocado</div>
-            <div class="navbar-burger" data-target="navMenu">
+            <div class="navbar-item">Lit</div>
+            <div class="navbar-burger" @click="toggleMenu" :class="{'is-active': isMenuActive}">
                 <span></span>
                 <span></span>
                 <span></span>
             </div>
         </div><!-- navbar-brand END -->
-        <div class="navbar-menu" id="navMenu">
+        <div class="navbar-menu" id="navMenu" :class="{'is-active': isMenuActive}">
             <div class="navbar-end">
                 <nuxt-link to="/" class="navbar-item">トップ</nuxt-link>
-                <nuxt-link to="/about" class="navbar-item">Avocadoとは？</nuxt-link>
-                <nuxt-link to="/contact" class="navbar-item">お問い合わせ</nuxt-link>
+                <nuxt-link to="/" class="navbar-item">お題作成</nuxt-link>
+                <nuxt-link to="/about" class="navbar-item">About Lit</nuxt-link>
+                <nuxt-link to="/contact" class="navbar-item">ლ(´ڡ`ლ)</nuxt-link>
             </div>
         </div><!-- navbar-menu END -->
     </nav>
 </template>
+
+<script>
+    export default {
+        data: () => {
+            return {isMenuActive: false}
+        },
+        methods: {
+            toggleMenu () {
+                this.isMenuActive = !this.isMenuActive
+            }
+        }
+    }
+</script>
