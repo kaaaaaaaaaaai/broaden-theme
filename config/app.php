38eb6fb4e32f1fef25da95b5ab2c493160701c3d
kai ogita <kai-ogita@rich.co.jp>
2018-05-02 10:36:35 +0900

    install-nuxt:laravel-cors追加

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Pbmedia\LaravelFFMpeg\FFMpegServiceProvider::class,
-
+        Barryvdh\Cors\ServiceProvider::class,
     ],
 
     /*
